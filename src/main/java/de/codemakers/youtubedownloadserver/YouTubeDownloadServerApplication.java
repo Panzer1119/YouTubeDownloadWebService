c7411a58074ffdbcd06629ccdc02aecccb7ab712
Paul Hagedorn <panzer1119@gmail.com>
2020-03-01 20:10:18 +0100

    Init
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

---
+++ src/main/java/de/codemakers/youtubedownloadserver/YouTubeDownloadServerApplication.java
@@ -0,0 +1,13 @@
+package de.codemakers.youtubedownloadserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouTubeDownloadServerApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(YouTubeDownloadServerApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/de/codemakers/youtubedownloadserver/YouTubeDownloadServerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.codemakers.youtubedownloadserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouTubeDownloadServerApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
