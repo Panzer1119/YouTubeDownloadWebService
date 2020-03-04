14755df8ee838e5e46577866dc989512a3cabeb6
Paul Hagedorn <panzer1119@gmail.com>
2020-03-04 09:59:53 +0100

    Edited YouTubeDownloadServerController.java Created download method to download a Video
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/codemakers/youtubedownloadserver/YouTubeDownloadServerController.java
+++ src/main/java/de/codemakers/youtubedownloadserver/YouTubeDownloadServerController.java
@@ -16,9 +16,17 @@
 
 package de.codemakers.youtubedownloadserver;
 
+import de.codemakers.io.file.AdvancedFile;
 import de.codemakers.youtubedownloadserver.security.AuthorizationUtil;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ZeroCopyHttpOutputMessage;
+import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
 
+import java.io.File;
 import java.time.ZonedDateTime;
 
 @RestController
@@ -27,19 +35,46 @@ public class YouTubeDownloadServerController {
     @RequestMapping(value = "/requesters/byTag/{tag}", method = RequestMethod.GET)
     public String getRequesterByTag(@PathVariable(value = "tag") String tag, @RequestParam(value = "authToken") String authToken) {
         if (!AuthorizationUtil.isValidToken(authToken)) {
-            return String.format("Unauthorized");
+            return String.format("Unauthorized"); //TODO How to return the right HttpStatus?
         }
         //TODO Return jsonObject form of the Requester object
         return String.format("{%n\"tag\":\"%s\",%n\"timestamp\":\"%s\"%n%n}", tag, ZonedDateTime.now().toString());
     }
     
-    @RequestMapping(value = "/requesters/{requester_id}", method = RequestMethod.GET)
-    public String getRequester(@PathVariable(value = "requester_id") int requesterId, @RequestParam(value = "authToken") String authToken) {
+    @RequestMapping(value = "/requesters/{requester.id}", method = RequestMethod.GET)
+    public String getRequester(@PathVariable(value = "requester.id") int requesterId, @RequestParam(value = "authToken") String authToken) {
         if (!AuthorizationUtil.isValidToken(authToken)) {
-            return String.format("Unauthorized");
+            return String.format("Unauthorized"); //TODO How to return the right HttpStatus?
         }
         //TODO Return jsonObject form of the Requester object
         return String.format("{%n\"requesterId\":%d,%n\"timestamp\":\"%s\"%n%n}", requesterId, ZonedDateTime.now().toString());
+    }
+    
+    @RequestMapping("/download/{video.id}")
+    public Mono<Void> download(ServerHttpResponse serverHttpResponse, @PathVariable(value = "video.id") String videoId, @RequestParam(value = "fileType", defaultValue = "B") String fileType, @RequestParam(value = "authToken") String authToken) {
+        if (!AuthorizationUtil.isValidToken(authToken)) {
+            serverHttpResponse.setStatusCode(HttpStatus.UNAUTHORIZED);
+            //TODO Does this work?
+            return null;
+        }
+        //FIXME Check if MediaFile for Video and File Type exist...
+        final AdvancedFile advancedFile = null; //TODO
+        final String filename = ""; //TODO
+        if (advancedFile == null || !advancedFile.exists()) {
+            serverHttpResponse.setStatusCode(HttpStatus.NO_CONTENT);
+            //TODO Does this work?
+            return null;
+        }
+        final ZeroCopyHttpOutputMessage zeroCopyHttpOutputMessage = (ZeroCopyHttpOutputMessage) serverHttpResponse;
+        serverHttpResponse.getHeaders().set(HttpHeaders.CONTENT_DISPOSITION, String.format("filename=\"%s\"", filename)); //FIXME Are these "" allowed there?
+        serverHttpResponse.getHeaders().setContentType(MediaType.APPLICATION_OCTET_STREAM);
+        final File file = advancedFile.toFile();
+        if (file == null) {
+            serverHttpResponse.setStatusCode(HttpStatus.INTERNAL_SERVER_ERROR);
+            //TODO Does this work?
+            return null;
+        }
+        return zeroCopyHttpOutputMessage.writeWith(file, 0, file.length());
     }
     
 }
