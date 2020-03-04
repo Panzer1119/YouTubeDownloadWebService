7c0f0cadfa65590c2f3f7ae6faf50329d3a240da
Paul Hagedorn <panzer1119@gmail.com>
2020-03-04 19:51:13 +0100

    Edited YouTubeDownloadServerController.java More AuthToken stuff
    Signed-off-by: Paul Hagedorn <panzer1119@gmail.com>

--- src/main/java/de/codemakers/youtubedownloadserver/YouTubeDownloadServerController.java
+++ src/main/java/de/codemakers/youtubedownloadserver/YouTubeDownloadServerController.java
@@ -17,8 +17,8 @@
 package de.codemakers.youtubedownloadserver;
 
 import de.codemakers.download.YouTubeDL;
+import de.codemakers.download.database.entities.AuthorizationToken;
 import de.codemakers.io.file.AdvancedFile;
-import de.codemakers.youtubedownloadserver.security.AuthorizationUtil;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -28,6 +28,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Mono;
 
 import java.io.File;
+import java.time.Duration;
 import java.time.ZonedDateTime;
 import java.util.List;
 
@@ -36,25 +37,27 @@ public class YouTubeDownloadServerController {
     
     @RequestMapping(value = "/requesters/byTag/{tag}", method = RequestMethod.GET)
     public String getRequesterByTag(@PathVariable(value = "tag") String tag, @RequestParam(value = "authToken") String authToken) {
-        if (!AuthorizationUtil.isValidToken(authToken)) {
+        if (!isValidToken(authToken)) {
             return String.format("Unauthorized"); //TODO How to return the right HttpStatus?
         }
+        useToken(authToken);
         //TODO Return jsonObject form of the Requester object
         return String.format("{%n\"tag\":\"%s\",%n\"timestamp\":\"%s\"%n%n}", tag, ZonedDateTime.now().toString());
     }
     
     @RequestMapping(value = "/requesters/{requester_id}", method = RequestMethod.GET)
     public String getRequester(@PathVariable(value = "requester_id") int requesterId, @RequestParam(value = "authToken") String authToken) {
-        if (!AuthorizationUtil.isValidToken(authToken)) {
+        if (!isValidToken(authToken)) {
             return String.format("Unauthorized"); //TODO How to return the right HttpStatus?
         }
+        useToken(authToken);
         //TODO Return jsonObject form of the Requester object
         return String.format("{%n\"requesterId\":%d,%n\"timestamp\":\"%s\"%n%n}", requesterId, ZonedDateTime.now().toString());
     }
     
     @RequestMapping("/download/{video_id}")
     public Mono<Void> download(ServerHttpResponse serverHttpResponse, @PathVariable(value = "video_id") String videoId, @RequestParam(value = "fileType", defaultValue = "B") String fileType, @RequestParam(value = "authToken") String authToken) {
-        if (!AuthorizationUtil.isValidToken(authToken)) {
+        if (!isValidToken(authToken)) {
             serverHttpResponse.setStatusCode(HttpStatus.UNAUTHORIZED);
             //TODO Does this work?
             return null;
@@ -76,15 +79,56 @@ public class YouTubeDownloadServerController {
             //TODO Does this work?
             return null;
         }
+        useToken(authToken);
         return zeroCopyHttpOutputMessage.writeWith(file, 0, file.length());
     }
     
     @RequestMapping("/download/videoIds/byPlaylistId/{playlist_id}")
     public List<String> downloadVideoIdsByPlaylistId(@PathVariable(value = "playlist_id") String playlistId, @RequestParam(value = "authToken") String authToken) {
-        if (!AuthorizationUtil.isValidToken(authToken)) {
+        if (!isValidToken(authToken)) {
             return null; //TODO How to return the right HttpStatus?
         }
+        useToken(authToken);
         return YouTubeDL.downloadVideoIdsFromURL(String.format("https://www.youtube.com/playlist?list=%s", playlistId));
+    }
+    
+    @RequestMapping("/generate/authorizationToken")
+    public String generateAuthorizationToken(@RequestParam(value = "unlimited", defaultValue = "0") boolean unlimited, @RequestParam(value = "granting", defaultValue = "0") boolean granting, @RequestParam(value = "duration", defaultValue = "100000") long durationMillis, @RequestParam(value = "authToken") String authToken) {
+        if (!isValidToken(authToken)) {
+            return String.format("Unauthorized"); //TODO How to return the right HttpStatus?
+        }
+        final AuthorizationToken authorizationTokenMaster = getAuthorizationToken(authToken);
+        if (authorizationTokenMaster == null) {
+            return String.format("Unauthorized"); //TODO How to return the right HttpStatus?
+        }
+        final Duration duration = Duration.ofMillis(durationMillis);
+        final AuthorizationToken authorizationTokenSlave;
+        if (granting) {
+            authorizationTokenSlave = authorizationTokenMaster.createGranterToken(duration);
+        } else {
+            if (unlimited) {
+                authorizationTokenSlave = authorizationTokenMaster.createUnlimitedToken(duration);
+            } else {
+                authorizationTokenSlave = authorizationTokenMaster.createSingleUseToken(duration);
+            }
+        }
+        if (authorizationTokenSlave == null || !YouTubeDownloadServer.useDatabaseOrFalse((database) -> database.addAuthorizationToken(authorizationTokenSlave))) {
+            return String.format("Unauthorized"); //TODO How to return the right HttpStatus?
+        }
+        useToken(authToken);
+        return authorizationTokenSlave.toJson();
+    }
+    
+    private static final boolean isValidToken(String token) {
+        return YouTubeDownloadServer.useDatabaseOrFalse((database) -> database.isValidAuthorizationToken(token));
+    }
+    
+    private static final AuthorizationToken getAuthorizationToken(String token) {
+        return YouTubeDownloadServer.useDatabaseOrNull((database) -> database.getAuthorizationTokenByToken(token));
+    }
+    
+    private static final boolean useToken(String token) {
+        return YouTubeDownloadServer.useDatabaseOrFalse((database) -> database.useAuthorizationToken(token));
     }
     
 }
